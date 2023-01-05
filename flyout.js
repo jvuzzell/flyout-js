6fc8bc9adfd41806a08b1352c8c8a9c69ff7dfa4
Joshua Uzzell <josh.uzzell@gmail.com>
2023-01-05 15:03:27 -0500

    - Update syntax for compatibility with ES6 modules - Refine how multiple expanded flyouts close - Adds transitionend event to flyout container to allow the closing animation to run before the attributes are removed from the body element

--- flyout.js
+++ flyout.js
@@ -1,14 +1,14 @@
-var flyoutPlugin = (function() {
+export let Flyouts = (function() {
 
-    var store = {};
+    let store = {};
 
-    var Constructor = function( options ) {
+    let Constructor = function( options ) {
 
-        var publicMethods = {};
-        var settings; 
+        let publicMethods = {};
+        let settings; 
 
         // Private
-        var expand = function( target ) {
+        const expand = ( target ) => {
             target.setAttribute( 'data-flyout-container', 'expanded' ); 
             publicMethods.updateState( { 'expanded' : true } );
 
@@ -16,31 +16,26 @@ var flyoutPlugin = (function() {
             document.querySelector( 'body' ).setAttribute( 'data-overlay', 'active' );
         }
 
-        var collapse = function( target ) {
+        const collapse = ( target ) => {
+
             target.setAttribute( 'data-flyout-container', 'collapsed' );
             publicMethods.updateState( { 'expanded' : false } );
-
-            multipleOpenFlyouts = document.querySelectorAll( '[data-flyout-container="expanded"]' );
-
-            if( multipleOpenFlyouts.length < 1 ) {
-                document.querySelector( 'body' ).setAttribute( 'data-flyout', 'inactive' );
-                document.querySelector( 'body' ).setAttribute( 'data-overlay', 'inactive' ); 
-                document.querySelector( 'body' ).setAttribute( 'data-overlay', 'inactive' ); 
-                document.querySelector( 'body' ).setAttribute( 'data-overlay', 'inactive' ); 
-            }
 
         }
 
         // Public
-
-        publicMethods.getSettings = function() {
+        publicMethods.getSettings = () => {
             return settings;
         }
 
-        publicMethods.toggle = function( e ) {
-            if( e !== undefined ) {
+        publicMethods.collapse = ( target ) => {
+            collapse( target );
+        }
+
+        publicMethods.toggle = ( event ) => {
+            if( event !== undefined ) {
                 // Check for field to update based on result of Flyout Transaction
-                settings.fieldToUpdate = ( e.target.getAttribute( 'data-target-update-field' ) == undefined ) ? null : e.target.getAttribute( 'data-target-update-field' ); 
+                settings.fieldToUpdate = ( event.target.getAttribute( 'data-target-update-field' ) == undefined ) ? null : event.target.getAttribute( 'data-target-update-field' ); 
             }
 
             if( settings.container.getAttribute( 'data-flyout-container' ) == 'collapsed' ) {
@@ -51,58 +46,54 @@ var flyoutPlugin = (function() {
 
         }
 
-        publicMethods.getFieldToUpdate = function() {
+        publicMethods.getFieldToUpdate = () => {
             return ( settings.fieldToUpdate == undefined ) ? null : settings.fieldToUpdate;  
         }
 
-        publicMethods.init = function( options ) {
+        publicMethods.init = ( options ) => {
             
             settings = options; // This makes arguments available in the scope of other methods within this object
 
             if( settings == null || settings == undefined ) { console.error( 'Flyout Plugin, settings not provided upon initialization' ); return; } 
 
-            var triggerSelector = settings.triggerSelector;
-            var triggerEvent    = 'click'; 
-            
+            let triggerSelector = settings.triggerSelector; 
+            let triggerCollection = document.querySelectorAll( triggerSelector );            
+
             if( settings.override === 'true' ) {
 
-                // Try to perform the custom callback, and handle errors gracefully
                 try {    
-                    window.addEventListener( triggerEvent, function( event ) {
-
-                        // Accomodate multiple triggers for one flyout
-                        var triggers = document.querySelectorAll( triggerSelector );
-
-                        // Narrow down trigger to exactly which trigger interaction
-                        for( var i = 0; i < triggers.length; i++) {
-                            if( event.target == triggers[ i ] ) {
-                                thisFlyoutSettings = flyoutPlugin.getFlyout( settings.id ).getSettings();
-                                window[ thisFlyoutSettings.customCallback ]( event );
-                            }
-                        }
-
-                    });          
+                    settings.callback = ( event ) => window[ thisFlyoutSettings.customCallback ]( event );
                 } catch( error ) {
                     console.error( 'Flyout Plugin, custom callback for Flyout ID: ' + settings.id + '  failed. Message: ' + error.message );
                 }
 
             } else {
 
-                window.addEventListener( triggerEvent, function( event ) {
-
-                    // Accomodate multiple triggers for one flyout
-                    var triggers = document.querySelectorAll( triggerSelector );
-
-                    // Narrow down trigger to exactly which trigger interaction
-                    for( var i = 0; i < triggers.length; i++) {
-                        if( event.target == triggers[ i ] ) {
-                            flyoutPlugin.getFlyout( settings.id ).toggle( event );
-                        }
-                    }
-
-                }); 
+                settings.callback = ( event ) => Flyouts.getFlyout( settings.id ).toggle( event );
 
             }
+
+            for( let i = 0; i < triggerCollection.length; i++ ) {
+                triggerCollection[ i ].setAttribute( 'data-trigger-id', i );
+                let triggerEvent = triggerCollection[ i ].getAttribute( 'data-flyout-trigger' ); 
+                triggerEvent = ( triggerEvent !== undefined && triggerEvent !== '' ) ? triggerEvent : 'click'; 
+                settings.triggerEvent = triggerEvent;
+
+                triggerCollection[ i ].addEventListener( triggerEvent, settings.callback );
+            }
+
+            function transitionEnd( event ) { 
+
+                let multipleOpenFlyouts = document.querySelectorAll( '[data-flyout-container="expanded"]' );
+  
+                if( multipleOpenFlyouts.length < 1 ) { 
+                        document.querySelector( 'body' ).setAttribute( 'data-flyout', 'inactive' );
+                        document.querySelector( 'body' ).setAttribute( 'data-overlay', 'inactive' );
+                }
+
+            }
+
+            settings.target.addEventListener( 'transitionend', transitionEnd ); 
 
             if( !settings.expanded ) {
                 collapse( settings.target );
@@ -133,68 +124,69 @@ var flyoutPlugin = (function() {
 
     }
 
-    var setFlyout = function( name, obj ) {
+    const setFlyout = function( name, obj ) {
         store[ name ] = obj;
     }
 
-    var getFlyout = function( name ) {
+    const getFlyout = function( name ) {
         return store[ name ];
     }
 	
-    var getFlyouts = function( name ) {
+    const getFlyouts = function( name ) {
         return store;
     }
 
-    var addCloseFlyoutEvents = function() {
+    const collapseAll = function() {
 
-        var flyoutParentContainer = document.querySelector( '.flyout-containers' );
+        let flyouts = Flyouts.getFlyouts();
 
-        // Collapse all containers if Fly-Container tag is clicked
-        // Allow users to exit flyout without having to click 'close' button
+        for( let key in flyouts ) { 
+
+            let flyout = flyouts[ key ]; 
+            flyout.collapse( flyout.getSettings().target );
+
+        }
+
+    }
+
+    const addCloseFlyoutEvents = function() {
+
+        let flyoutParentContainer = document.querySelector( '.flyout-containers' );
+   
         window.addEventListener( 'click', function( event ) {
 
-            var closeTrigger = event.target.getAttribute( 'data-close-flyout' ); 
-            
-            // If you click in the gray overlay space (the main flyout parent container), collapse every flyout.
-            if( event.target == flyoutParentContainer ) { 
-                var flyouts = document.querySelectorAll( '[data-flyout-container]' );
-
-                for( var i = 0; i < flyouts.length; i++ ) {
-                    var flyout = flyoutPlugin.getFlyout( flyouts[i].getAttribute('data-flyout-target') );
-
-                    if( flyout.isExpanded() ) {
-                        flyout.toggle();
-                    }
-                }     
-
-                document.querySelector( 'body' ).setAttribute( 'data-flyout', 'inactive' );
-                document.querySelector( 'body' ).setAttribute( 'data-overlay', 'inactive' ); 
+            // Collapse all containers if Fly-Container tag is clicked
+            // Allow users to exit flyout without having to click 'close' button
+            if( event.target === flyoutParentContainer ) { 
+                Flyouts.collapseAll();
+                return;
             }
-
+            
+            let closeTrigger = event.target.getAttribute( 'data-close-flyout' ); 
             // If you click the X button, close that single flyout.
             if( closeTrigger !== null ) { 
-                flyoutPlugin.getFlyout( closeTrigger ).toggle();
+                Flyouts.getFlyout( closeTrigger ).toggle();
             }
 
         });
 
     }
 
-    var registerFlyout = function( flyout, iterator ) {
+    const registerFlyout = function( flyout, iterator ) {
 
-        var flyoutTarget          = flyout;
-        var flyoutName            = flyoutTarget.getAttribute( 'data-flyout-target' );
-        var flyoutTriggerSelector = '[data-flyout-target="' + flyoutName + '"][data-flyout-trigger]';
-        var flyoutTriggers        = document.querySelectorAll( flyoutTriggerSelector );
-        var isExpanded            = ( flyout.getAttribute( 'data-flyout-container' ) == 'collapsed' ) ? false : true;
-        var flyoutCallback        = null;
-        var callbackName;
+        let flyoutTarget          = flyout;
+        let flyoutName            = flyoutTarget.getAttribute( 'data-flyout-target' );
+        let flyoutTriggerSelector = '[data-flyout-target="' + flyoutName + '"][data-flyout-trigger]';
+        let flyoutTriggers        = document.querySelectorAll( flyoutTriggerSelector );
+        let isExpanded            = ( flyout.getAttribute( 'data-flyout-container' ) == 'collapsed' ) ? false : true;
+        let flyoutCallback        = null;
+        let callbackName;
 
         // flyoutCallback will be called if you set flyoutOverride to 'true' (string val).
         // The first (and only) argument to flyoutCallback will be e (the click event).
         // If you do this, you will need to open the flyout on your own. You will also need to obtain the field to update.
         // See toggle() above for how to do this. Or, you could just call toggle() on your own and pass e along to it.
-        var flyoutOverride = flyout.getAttribute( 'data-flyout-override' );
+        let flyoutOverride = flyout.getAttribute( 'data-flyout-override' );
 
         if( flyoutOverride && flyoutOverride !== 'false' && flyoutOverride !== 'custom' ) {
             callbackName = flyout.getAttribute( 'data-flyout-callback' ); 
@@ -205,10 +197,10 @@ var flyoutPlugin = (function() {
         if( flyoutTarget == null ) { console.warn( 'Flyout Plugin did not detect target, Node:', flyout ); }
         if( flyoutTriggers.length == 0 ) { console.warn( 'Flyout Plugin did not detect trigger, Node:', flyout) ; }
 
-        // As you instantiate new flyoutPlugins, insert them in the flyoutStore object, indexed by the name of the flyout.
+        // As you instantiate new Flyouts, insert them in the flyoutStore object, indexed by the name of the flyout.
         this.storeFlyout(
             flyoutName,
-            new flyoutPlugin.launch({
+            new Flyouts.launch({
                 id               : flyoutName,
                 container        : flyoutTarget, 
                 triggerSelector  : flyoutTriggerSelector, 
@@ -232,30 +224,21 @@ var flyoutPlugin = (function() {
         storeFlyout          : setFlyout, 
         getFlyout            : getFlyout, 
 		getFlyouts           : getFlyouts,
-        addCloseFlyoutEvents : addCloseFlyoutEvents
+        addCloseFlyoutEvents : addCloseFlyoutEvents, 
+        collapseAll          : collapseAll
     };  
   
 })();
 
-(function() {
-
-    var initFlyouts = function() {
-        
-        var flyouts = document.querySelectorAll( '[data-flyout-container]' );
-        if( flyouts == null ) return; 
+export const initFlyouts = function() {
     
-        for( var i = 0; i < flyouts.length; i++ ) {
-            flyoutPlugin.registerFlyout( flyouts[ i ], i );
-        }
+    let flyouts = document.querySelectorAll( '[data-flyout-container]' );
+    if( flyouts == null ) return; 
 
-        flyoutPlugin.addCloseFlyoutEvents();
-        
+    for( var i = 0; i < flyouts.length; i++ ) {
+        Flyouts.registerFlyout( flyouts[ i ], i );
     }
 
-    if( typeof runOnAppStart == "function" ) {
-        runOnAppStart( 'initializeFlyouts', initFlyouts );
-    } else { 
-        initFlyouts();
-    }    
-
-})();
+    Flyouts.addCloseFlyoutEvents();
+    
+}
